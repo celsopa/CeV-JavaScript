f1c28c740eb4c846d43fddbc69ca6ce47ff16e1d
celsopa <celsopa@msn.com>
2019-05-31 12:01:47 -0300

    Attualização da aula 15

--- modulo_F/aula15/ambiente_vetores-na-tela.js
+++ modulo_F/aula15/ambiente_vetores-na-tela.js
@@ -1,10 +1,18 @@
-let lista = ['a', 'b','c','d','e','f','g','h','i','j','k', 'l', 'm','n','o','p', 'q', 'r', 's', 't', 'u', 'v', 'x','y', 'w','z']
-console.log(`A lista ${lista} tem ${lista.length} elementos.`)
+let lista1 = ['a', 'b','c','d','e','f','g','h','i','j','k', 'l', 'm','n','o','p', 'q', 'r', 's', 't', 'u', 'v', 'x','y', 'w','z']
+let lista2 = [2,9,4,7,6,5,8,3,0,1]
+console.log(`A lista ${lista2} tem ${lista2.length} elementos.`)
+console.log(`A lista ${lista1} tem ${lista1.length} elementos.`)
+
 // console.log('--------------------------------------------------------------------------------')
 // for (let i = 0; i < lista.length ; i++){
 //     console.log(`Índice ${i} -> ${lista[i]}`)
 // }
+
 console.log('--------------------------------------------------------------------------------')
-for (var x in lista){
-    console.log(`Índice ${x} -> ${lista[x]}`)
+for (var x in lista2){
+    console.log(`Índice ${x} -> ${lista2[x]}`)
 }
+
+// for (var x in lista1){
+//     console.log(`Índice ${x} -> ${list1a[x]}`)
+// }
