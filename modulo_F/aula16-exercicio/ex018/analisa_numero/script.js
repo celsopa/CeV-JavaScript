b1bf58375b648aea4ad4dccef3dcff7822d4b682
celsopa <celsopa@msn.com>
2019-05-31 17:10:01 -0300

    Att aula 16 e exercício

---
+++ modulo_F/aula16-exercicio/ex018/analisa_numero/script.js
@@ -0,0 +1,85 @@
+var txtnum = document.getElementById("numero")
+
+var lista = document.getElementById("lista_num")
+var res = document.getElementById("resultado")
+
+function carregar(){
+    txtnum.focus()
+}
+
+function adicionar(){
+    var num = Number(txtnum.value)
+    var lista_num = document.getElementsByTagName("option")
+    if (txtnum.value <= 100 && txtnum.value > 0){
+        if (lista_num.length == 0) {
+            var opt = document.createElement("option")
+            opt.text = `Número: ${num}`
+            opt.value =`${num}`
+            lista.appendChild(opt)
+            limpar()
+        } else {
+            var achou = 0
+            for (let i = 0; i < lista_num.length; i++) {
+                if (num == Number(lista_num[i].value)) {
+                    achou++
+                }
+            }
+            if (achou == 0){
+                var opt = document.createElement("option")
+                opt.text = `Número: ${num}`
+                opt.value =`${num}`
+                lista.appendChild(opt)
+                limpar()
+            } else {
+                alert("[ERRO] Número já adiconado!")
+            }
+        }
+    } else {
+        alert("[ERRO] Valor inválido!")
+    }
+}
+
+function analisar(){
+    var lista_num = document.getElementsByTagName("option")
+    var soma = 0
+    var maior = 0
+    var menor = 0
+    var media = 0
+    if (lista_num.length == 0){
+        alert("[ERRO] Informe ao menos um valor!")
+    } else{
+        for (let c = 0; c < lista_num.length; c++){
+            if (c==0){
+                maior = lista_num[c].value
+                menor = lista_num[c].value 
+            } else {
+                if (lista_num[c].value > maior){
+                    maior = lista_num[c].value
+                }
+                if (lista_num[c].value < menor){
+                    menor = lista_num[c].value
+                }
+            }
+            soma += Number(lista_num[c].value)
+        }
+        media = soma/lista_num.length
+        res.innerHTML = `<p>Você digitou ${lista_num.length} números.</p>
+        <p>O maior número digitado foi ${maior}</p>
+        <p>O menor número digitado foi ${menor}</p>
+        <p>A soma de todos os númeors digitados é ${soma}</p>
+        <p>A média de todos os númeors digitados é ${media}</p>`
+    }
+}
+
+function limpar(n=0){
+    if (n) {
+        lista.innerHTML = ""
+        res.innerHTML = ""
+        txtnum.value = ""
+        txtnum.focus()
+    } else {
+        res.innerHTML = ""
+        txtnum.value = ""
+        txtnum.focus()
+    }
+}
---
+++ modulo_F/aula16/funcao01.js
@@ -0,0 +1,10 @@
+function parimpar(n){
+    if (n % 2 == 0){
+        return `${n} é PAR`
+    } else {
+        return `${n} é ÍMPAR`
+    }
+}
+
+
+console.log(parimpar(3))
